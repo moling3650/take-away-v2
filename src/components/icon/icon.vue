609cd1f1324778043ac6c373d5437047f5f3f83d
moling3650 <365024424@qq.com>
2017-01-03 17:03:13 +0800

    开发完成icon组件

---
+++ src/components/icon/icon.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="icon" :class="pic"></span>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props: {
+    pic: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../common/stylus/mixin'
+
+.icon
+  display inline-block
+  background-repeat no-repeat
+  &.brand
+    bg-image('brand', 30px, 18px)
+  &.bulletin
+    bg-image('bulletin', 22px, 12px)
+  for $pic in decrease discount guarantee invoice special
+    for $i in 1..4
+      &.{$pic}_{$i}
+        if $i % 2 == 1
+          bg-image(s('%s_%s', $pic, $i), 12px, 12px)
+        else
+          bg-image(s('%s_%s', $pic, $i), 16px, 16px)
+</style>
