114d75f7608f120a71402515754c90bc6f07b18c
moling3650 <365024424@qq.com>
2017-01-03 14:03:11 +0800

    添加border-bottom 1px solid color的通用实现

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <v-header></v-header>
     <div class="tab">
-      <div class="tab-item">
+      <div class="tab-item border-1px">
         <router-link to="/goods">商品</router-link>
       </div>
       <div class="tab-item">
@@ -26,12 +26,15 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import 'common/stylus/mixin'
+
 #app
   .tab
     display flex
     width 100%
     height 40px
     line-height 40px
+    border-1px(rgba(7, 17, 27, .1))
     .tab-item
       flex 1
       text-align center
